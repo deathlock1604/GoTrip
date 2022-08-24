c0d5dcd31fdb0a441b6157c16b3e8c67c5cf30c8
deathlock1604 <110537330+deathlock1604@users.noreply.github.com>
2022-08-24 23:03:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Carousel
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 20,
+  startPosition: 1,
+  items: 1,
+  responsive: {
+    540: {
+      items: 3,
+      startPosition: 0,
+    },
+
+    1200: {
+      items: 3,
+      margin: 30,
+    },
+  },
+});
+
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+// Nav Icon
+
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon--active");
+  document.body.classList.toggle("no-scroll");
+};
